2df36bc932f2db3350c6d840c74016a36736f613
DhavalAjudiya <dhavalajudiya9469@gmail.com>
2022-03-11 14:24:33 +0530

    FIRST

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'widget/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        hoverColor: Colors.transparent,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialBinding: AppBiding(),
+      initialRoute: SplashScreen.routeName,
+      getPages: [
+        GetPage(
+            name: SplashScreen.routeName,
+            page: () => SplashScreen(),
+            transition: Transition.rightToLeft),
+      ],
+    );
+  }
+}
+
+class AppBiding implements Bindings {
+  @override
+  void dependencies() {}
+}
