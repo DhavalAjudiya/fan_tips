b081400611c9865fc4959f100c41e9b0c9dc0154
DhavalAjudiya <dhavalajudiya9469@gmail.com>
2022-03-12 09:57:12 +0530

    change

--- lib/utills/asset.dart
+++ lib/utills/asset.dart
@@ -1,7 +1,7 @@
 class AppImage {
   static const splashImage = "assets/image/splash_screen.png";
-  // static const String image = 'asset/images';
-  // static const String splashScreen = "$image/splash_screen.png";
+  static const String image = 'assets/images';
+  static const String splashScreen = "$image/splash_screen.png";
 }
 
 class IconAsset {
