981a420b112ee548f03044d04f4a9a10e0c0b48b
DhavalAjudiya <dhavalajudiya9469@gmail.com>
2022-03-22 16:41:59 +0530

    bug fix

--- lib/matches/page/current_matches.dart
+++ lib/matches/page/current_matches.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:fantips/commanWidget/commanText.dart';
 import 'package:fantips/matches/widget/upcoming.dart';
 import 'package:fantips/utills/string.dart';
+import 'package:fantips/widget/app_bottom_sheet.dart';
 import 'package:fantips/widget/google_sign_in_repo.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,7 @@ import '../../T20Predictions/page/utills/asset.dart';
 import '../../T20Predictions/page/utills/color.dart';
 import '../../expert/data/controller.dart';
 import '../../screen/ipl_screen/widget/fantasyTabBar/wight/container/container_custom.dart';
+import '../../widget/header_row.dart';
 import '../../widget/profile_screen.dart';
 import '../../utills/string.dart';
 import '../../widget/google_sign_in_repo.dart';
@@ -94,108 +96,7 @@ class MatchesScreen extends StatelessWidget {
                     Obx(
                       () => InkWell(
                         onTap: () {
-                          showModalBottomSheet(
-                            backgroundColor: Colors.black,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                                topRight: Radius.circular(6.w),
-                                topLeft: Radius.circular(6.w),
-                              ),
-                            ),
-                            context: context,
-                            builder: (context) => Column(
-                              children: [
-                                Row(
-                                  children: [
-                                    IconButton(
-                                      onPressed: () {
-                                        Get.back();
-                                      },
-                                      icon: Icon(
-                                        Icons.clear,
-                                        size: 9.w,
-                                      ),
-                                      splashColor: AppColor.transparent,
-                                    ),
-                                    SizedBox(
-                                      height: 3.h,
-                                    ),
-                                  ],
-                                ),
-                                Image.asset(AppImage.logo, height: 40.w),
-                                Padding(
-                                  padding: EdgeInsets.all(5.w),
-                                  child: Row(
-                                    children: [
-                                      Container(
-                                        height: 0.2.h,
-                                        width: 27.w,
-                                        color: AppColor.grey,
-                                      ),
-                                      SizedBox(
-                                        width: 2.w,
-                                      ),
-                                      const CustomeText(
-                                          title: AppString.letsconnect),
-                                      SizedBox(
-                                        width: 2.w,
-                                      ),
-                                      Container(
-                                        height: 0.2.h,
-                                        width: 27.w,
-                                        color: AppColor.grey,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 2.h,
-                                ),
-                                AppContainer(
-                                  height: 6.h,
-                                  width: 60.w,
-                                  color: AppColor.containerBackground,
-                                  borderRadius: BorderRadius.circular(10),
-                                  onTap: () {
-                                    signInWithGoogle().then((data) {
-                                      ipController.isLoggedIn.value = true;
-                                      ipController.userObj = data;
-                                    }).catchError((e) {});
-                                  },
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Image.asset(AppImage.google, height: 8.w),
-                                      const CustomeText(
-                                        title: AppString.googleSign,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 2.h,
-                                ),
-                                AppContainer(
-                                  height: 6.h,
-                                  width: 60.w,
-                                  color: AppColor.containerBackground,
-                                  borderRadius: BorderRadius.circular(10),
-                                  onTap: () {},
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Image.asset(AppImage.fb, height: 8.w),
-                                      const CustomeText(
-                                        title: AppString.fbLogin,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
+                          AppBottomSheet().bottomSheet(context);
                         },
                         child: ipController.isLoggedIn.value == false
                             ? CustomeText(
@@ -222,9 +123,6 @@ class MatchesScreen extends StatelessWidget {
                     ),
                   ],
                 ),
-                // HeaderRow(
-                //   title: AppString.title,
-                // ),
                 SizedBox(height: 0.5.h),
                 TabBar(
                   automaticIndicatorColorAdjustment: true,
